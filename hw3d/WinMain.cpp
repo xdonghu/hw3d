79e65e3358187e9f5060d1c6128e76a694a20f33
hxd <hxd@radardo.com>
2021-01-16 13:20:02 +0800

    hxd20210116 创建hw3d库，创建window，提交。

---
+++ hw3d/WinMain.cpp
@@ -0,0 +1,71 @@
+#include <Windows.h>
+
+
+// 自定义消息函数[14/1/2021 HXD]
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch ( msg )
+	{
+	case WM_CLOSE:
+		PostQuitMessage(69);
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+int CALLBACK WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nShowCmd)
+{
+
+	const auto pClassName = "hw3dbutts";
+
+	// 注册窗口类[14/1/2021 HXD]
+	WNDCLASSEX wc = {0};
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx( &wc );
+
+	// 创建窗口实例[14/1/2021 HXD] 
+	HWND hWnd = CreateWindowEx(
+		0, pClassName,
+		"Happy Hard Window",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr, nullptr, hInstance, nullptr
+	);
+
+	// 显示窗口[14/1/2021 HXD]
+	ShowWindow(hWnd, SW_SHOW);
+
+	// 消息循环[14/1/2021 HXD]
+	MSG msg;
+	BOOL gResult;
+	while ( gResult = GetMessage( &msg, nullptr, 0, 0) > 0 )
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if ( gResult == -1) 
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
+
+}
